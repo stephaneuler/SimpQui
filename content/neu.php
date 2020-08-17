64812a2a6c8717292d76513e701adfbc997aabed
Stephan Euler <stephan.euler@mnd.thm.de>
2020-08-17 09:21:58 +0200

    removed bug in neu

--- content/neu.php
+++ content/neu.php
@@ -6,7 +6,7 @@ usort($files, function($a, $b) {
     return filemtime($a) < filemtime($b);
 });
 echo '<table class="table table-hover">';
-for( $i=0; $i<6; $i++ ) {
+for( $i=0; $i<6 & $i < count( $files ); $i++ ) {
 	$t = str_replace(".txt","", $files[$i]);
 	$t = str_replace("topics/","", $t);
 	$time = date('F d, H:i',filemtime($files[$i]));
